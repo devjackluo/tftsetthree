3b9616f8c3564c5e16f128fe40bf3e5d8ff241c6
devjackluo <jackluo94@hotmail.com>
2020-06-16 02:20:26 -0700

    Updates

---
+++ precache-manifest.8ceca6ef4a91e183ba105a4d31d46897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618fc08952c6496132a1344933c8b192",
+    "url": "/tftsetthree/index.html"
+  },
+  {
+    "revision": "9fd0a4e87e6cac308eb9",
+    "url": "/tftsetthree/static/css/main.39bd69d1.chunk.css"
+  },
+  {
+    "revision": "cff8f9264ddc8e54667a",
+    "url": "/tftsetthree/static/js/2.004138d9.chunk.js"
+  },
+  {
+    "revision": "9fd0a4e87e6cac308eb9",
+    "url": "/tftsetthree/static/js/main.9f613690.chunk.js"
+  },
+  {
+    "revision": "1308d939ac7e2e631333",
+    "url": "/tftsetthree/static/js/runtime-main.2727c73a.js"
+  }
+]);
